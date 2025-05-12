6557be47bde2bd025e8f23f74251239143a5b84a
vctrubio <vctrubio@gmail.com>
2025-05-12 12:17:08 +0200

    AntdRegistry npm install for nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 import ConvexClientProvider from "@/components/ConvexClientProvider";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 
 
 export const metadata: Metadata = {
@@ -18,13 +19,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ConvexAuthNextjsServerProvider>
-      <html lang="en">
-        <body
-        >
-          <ConvexClientProvider>{children}</ConvexClientProvider>
-        </body>
-      </html>
-    </ConvexAuthNextjsServerProvider>
+    <AntdRegistry>
+      <ConvexAuthNextjsServerProvider>
+        <html lang="en">
+          <body
+          >
+            <ConvexClientProvider>{children}</ConvexClientProvider>
+          </body>
+        </html>
+      </ConvexAuthNextjsServerProvider>
+    </AntdRegistry>
   );
 }
