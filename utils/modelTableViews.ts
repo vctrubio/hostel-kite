313f187e327fc18348847da5196f27423add2ddf
vctrubio <vctrubio@gmail.com>
2025-05-12 14:36:27 +0200

    genericTableView init

---
+++ utils/modelTableViews.ts
@@ -0,0 +1,12 @@
+//key to module name, value to columns for tables
+export const genericTableView = {
+  students: {
+    fullName: "string",
+    age: "number",
+    languages: "array",
+  },
+  teachers: {
+    fullName: "string",
+    languages: "array",
+  },
+};
