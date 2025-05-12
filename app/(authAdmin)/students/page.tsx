77a1f3e68b1614d4a4df50cb5dd7fed2bb2323db
vctrubio <vctrubio@gmail.com>
2025-05-12 13:36:41 +0200

    Init of students page

--- app/(authAdmin)/students/page.tsx
+++ app/(authAdmin)/students/page.tsx
@@ -1,12 +1,7 @@
-"use client";
-
-
 export default function StudentsPage() {
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Students Management</h1>
-      <div className="bg-white dark:bg-slate-800 p-6 rounded-lg shadow">
-      </div>
     </main>
   );
 }
